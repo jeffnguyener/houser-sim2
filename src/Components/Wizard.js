9538ba8ee8f050114297b80806068b279524c70f
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-24 16:50:53 -0600

    update wiz

--- src/Components/Wizard.js
+++ src/Components/Wizard.js
@@ -1,61 +1,70 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Wizard extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-
+            propertyName: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: ""
         }
     }
+    handleNewPropName(){
+        
+    }
+    
     resetForm = () => {
         this.setState()
-      }
+    }
 
     render() {
         return (
             <div>
                 <button onClick={this.resetForm}
-                        type="button">Cancel</button>
-            <div className="propertyname">
-                <h3>Property Name</h3>
-            <input type="text"
-                    placeholder="Property Name"
-                // onChange={}
-                // value={} 
-                />
-            </div>
-            <div className="address">
-                <h3>Address</h3>
-                <input type="text"
-                    placeholder="Address"
-                // onChange={}
-                // value={}
-                />
-            </div>
-            <div className="city">
-                <h3>City</h3>
-                <input type="text"
-                    placeholder="Address"
-                // onChange={}
-                // value={}
-                />
-            </div>
-            <div className="state">
-                <h3>State</h3>
-                <input type="text"
-                    placeholder="Address"
-                // onChange={}
-                // value={}
-                />
-            </div>
-            <div className="zip">
-                <h3>Zipcode</h3>
-                <input type="text"
-                    placeholder="Address"
-                // onChange={}
-                // value={}
-                />
-            </div>
+                    type="button">Cancel</button>
+                <div className="propertyname">
+                    <h3>Property Name</h3>
+                    <input type="text"
+                        placeholder="Property Name"
+                    // onChange={}
+                    // value={} 
+                    />
+                </div>
+                <div className="address">
+                    <h3>Address</h3>
+                    <input type="text"
+                        placeholder="Address"
+                    // onChange={}
+                    // value={}
+                    />
+                </div>
+                <div className="city">
+                    <h3>City</h3>
+                    <input type="text"
+                        placeholder="Address"
+                    // onChange={}
+                    // value={}
+                    />
+                </div>
+                <div className="state">
+                    <h3>State</h3>
+                    <input type="text"
+                        placeholder="Address"
+                    // onChange={}
+                    // value={}
+                    />
+                </div>
+                <div className="zip">
+                    <h3>Zipcode</h3>
+                    <input type="text"
+                        placeholder="Address"
+                    // onChange={}
+                    // value={}
+                    />
+                </div>
                 <button>Complete</button>
             </div>
         )
