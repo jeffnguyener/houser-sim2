4e751b6f2c8be406840bbd00cda37354fd875070
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-24 11:15:58 -0600

    update components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './Components/Dashboard';
+import Wizard from './Components/Wizard'
+import Header from './Components/Header'
+
 
 function App() {
   return (
     <div className="App">
-     
+        <Dashboard />
+        <Wizard />
+        <Header />
     </div>
   );
 }
